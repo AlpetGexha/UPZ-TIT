9c447a344c654e54ee1ed4c4ff963d971a5d25f7
AlpetGexha <50520333+AlpetGexh@users.noreply.github.com>
2022-10-18 09:44:26 +0200

    Java 3 Ushtrime

---
+++ Semestri-1/Programim/Java_3/Ushtrime/Karakteret.java
@@ -0,0 +1,12 @@
+public class Karakteret {
+    public static void main(String[] args) {
+        System.out.println('a');    // a
+
+        // S = 83, i = 105, rez 83 + 105 = 188
+        System.out.println('S' + 'i'); // 188
+
+        System.out.println('E' * 5); // 69 * 5 = 345
+
+        System.out.println("Univesiteti \" Ukshin Hoti \" " );
+    }
+}
---
+++ Semestri-1/Programim/Java_3/Ushtrime/Ushtrimi_1.java
@@ -0,0 +1,7 @@
+public class Ushtrimi_1 {
+    public static void main(String []args){
+        System.out.print("Programi ");
+        System.out.print("i pare ");
+        System.out.println("ne JAVA ");
+    }
+}
