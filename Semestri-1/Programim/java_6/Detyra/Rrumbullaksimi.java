c1f13b3edd5af89ee84c4ebcd2ecf37b2cb20540
AlpetGexha <50520333+AlpetGexh@users.noreply.github.com>
2022-11-08 17:13:35 +0100

    Java 6 Detyrat

---
+++ Semestri-1/Programim/java_6/Detyra/Rrumbullaksimi.java
@@ -0,0 +1,38 @@
+package java6;
+
+import java.util.Scanner;
+
+public class Rrumbullaksimi {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Jep vlerën a: ");
+        double a = s.nextDouble();
+
+        System.out.print("Jep vlerën b: ");
+        double b = s.nextDouble();
+
+        double result = a / b;
+        double result1 = result;
+        int rrumbullasimi;
+
+        /*
+         * Na duhet ta dijm sa e kemi mbetjen
+         * Llogaritja:
+         *
+         * Numri me mbetje - Numri i plotë = Mbetja
+         *
+         * Nese mbetja eshte me e madhe se 0.5 atehere rrumbullasimi eshte numri i plotë + 1
+         * Nese mbetja eshte me e vogel se 0.5 atehere rrumbullasimi eshte numri i plotë
+         * */
+        if ((result1 - (int) result1) >= .5)
+            rrumbullasimi = (int) result + 1;
+        else
+            rrumbullasimi = (int) result;
+
+
+        System.out.println(String.format("Rrumbullasimi i %.4f eshte %s", result, rrumbullasimi));
+
+    }
+}
