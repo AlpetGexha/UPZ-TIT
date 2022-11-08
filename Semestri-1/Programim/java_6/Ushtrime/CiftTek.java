43d3ecab7d6974bc8b303c6b923e1c7eceecdc77
AlpetGexha <50520333+AlpetGexh@users.noreply.github.com>
2022-11-08 15:40:22 +0100

    Java 6 Ushtrime

---
+++ Semestri-1/Programim/java_6/Ushtrime/CiftTek.java
@@ -0,0 +1,14 @@
+package java6;
+
+import java.util.Scanner;
+
+public class CiftTek {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        String res = (n % 2 == 0) ? "Çift" : "Tek";
+
+        System.out.println(String.format("Numri %d eshte %s", n, res));
+    }
+
+}
---
+++ Semestri-1/Programim/java_6/Ushtrime/PozitivNegativ.java
@@ -0,0 +1,16 @@
+package java6;
+
+import java.util.Scanner;
+
+public class PozitivNegativ {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Jep Numrin: ");
+        int n = s.nextInt();
+
+       String res = (n < 0) ? "Negativ" : "Pozitiv";
+
+         System.out.println(String.format("Numri %d eshte %s", n, res));
+    }
+}
