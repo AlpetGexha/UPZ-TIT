ded46c0094ff148a8e463d5b555bd0340572cbd3
Alpet Gexha <50520333+AlpetGexh@users.noreply.github.com>
2022-12-21 23:50:04 +0100

    Provimi i Janarit 2020

---
+++ Semestri-1/Programim/ProvimiJanar2020/Detyra_1_loop.java
@@ -0,0 +1,4 @@
+package ProvimiJanar2020;
+
+public class Detyra_1_loop {
+}
---
+++ Semestri-1/Programim/ProvimiJanar2020/Detyra_5_Armstorng.java
@@ -0,0 +1,39 @@
+package ProvimiJanar2020;
+
+import java.util.Scanner;
+
+/**
+ * Një numër i Armstrongut është një numër i plotë pozitiv që është i
+ * barabartë me shumën e shifrave të tij të ngritur në fuqinë e numrit të shifrave.
+ * Për shembull, 371 ka tre shifra, 3^3 + 7^3 + 1^3 = 27 + 343 + 1 = 371.
+ * Shkruani një program që pranon një numër të plotë pozitiv dhe përcakton nëse numri është i Armstrongut.
+ */
+public class Detyra_5_Armstorng {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Jep nje numer pozitiv: ");
+        int n = s.nextInt();
+        int tmp = n;
+        int org = n;
+        int numri_i_shifrafe = 0;
+
+        while (tmp > 0) {
+            numri_i_shifrafe++;
+            tmp /= 10;
+        }
+
+        int sum = 0;
+        while (n > 0) {
+            int sh = n % 10;
+            sum += Math.pow(sh, numri_i_shifrafe);
+            n /= 10;
+        }
+        System.out.println(sum);
+
+        if (org == sum)
+            System.out.print("Armstrongut");
+        else
+            System.out.print("Nuk osht Armstrongut");
+    }
+}
