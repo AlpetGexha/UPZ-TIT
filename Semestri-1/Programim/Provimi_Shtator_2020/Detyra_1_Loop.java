dfc0ccd8c25cfbc4461850de4ccd6f4db7d1eeb9
Alpet Gexha <50520333+AlpetGexh@users.noreply.github.com>
2022-12-21 23:49:52 +0100

    Provimi i Shtatorit 2020

---
+++ Semestri-1/Programim/Provimi_Shtator_2020/Detyra_1_Loop.java
@@ -0,0 +1,20 @@
+package Provimi_Shtator_2020;
+
+/**
+ *
+ * (20 pikë) Shkruani një program në metodën main() i cili pasi të jetë ekzekutuar
+ * do të ketë këtë dalje:
+ *
+                    x o o o x
+                    x x o x x
+                    x x x x x
+                    x x o x x
+                    x o o o x
+ *
+ * */
+
+public class Detyra_1_Loop {
+    public static void main(String[] args) {
+
+    }
+}
