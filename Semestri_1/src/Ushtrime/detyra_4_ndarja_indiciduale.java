28c4013b7448ab8571a11b6080e820e9427b383b
Alpet Gexha <50520333+AlpetGexh@users.noreply.github.com>
2023-01-18 19:50:57 +0100

    indicidulae

---
+++ Semestri_1/src/Ushtrime/detyra_4_ndarja_indiciduale.java
@@ -0,0 +1,25 @@
+package Ushtrime;
+
+public class detyra_4_ndarja_indiciduale {
+    public static void main(String[] args) {
+        int n = 312312;
+        int tmp = n;
+        int count = 0;
+
+        while (tmp > 0) {
+            count++;
+            tmp /= 10;
+        }
+
+        int[] vargu = new int[count];
+        for (int i = 0; i < vargu.length; i++) {
+            vargu[i] = n % 10;
+            n /= 10;
+        }
+
+        for (int i = vargu.length - 1 ; i >= 0; i--) {
+            System.out.print(vargu[i]);
+        }
+
+    }
+}
