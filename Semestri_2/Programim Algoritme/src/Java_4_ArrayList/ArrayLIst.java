c0fc415d6a38f6e689c2c3f200b82165018adb5d
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2023-03-14 21:29:26 +0100

    Array List

---
+++ Semestri_2/Programim Algoritme/src/Java_4_ArrayList/ArrayLIst.java
@@ -0,0 +1,48 @@
+package Java_4_ArrayList;
+
+public class ArrayLIst {
+
+    int[] array;
+    int size, maxSize;
+
+    public void ArrayLIst(int maxSize) {
+        maxSize = maxSize;
+        size = 0;
+        array = new int[maxSize];
+    }
+
+    public void add(int value) {
+        if (size < maxSize) {
+            array[size] = value;
+            size++;
+        }
+    }
+
+    public int find(int value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void remove(int value) {
+        int index = find(value);
+        if (index != -1) {
+            for (int i = index; i < size - 1; i++) {
+                array[i] = array[i + 1];
+            }
+            size--;
+        }
+    }
+    public void display() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+
+
+
+}
---
+++ Semestri_2/Programim Algoritme/src/Java_4_ArrayList/Main.java
@@ -0,0 +1,12 @@
+package Java_4_ArrayList;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+//        ArrayList<Integer> arr = new ArrayList<Integer>();
+        ArrayLIst arr = new ArrayLIst(8);
+
+    }
+
+}
