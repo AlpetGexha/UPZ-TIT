ba8b47ba938a5ed6928ad70f76cc71ff2ecc9a2a
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2023-04-04 00:28:28 +0200

    Linked List Builder Experiment

--- Semestri_2/Programim Algoritme/src/LikendListBuilderPattern/App.java
+++ Semestri_2/Programim Algoritme/src/LikendListBuilderPattern/App.java
@@ -3,16 +3,20 @@ package LikendListBuilderPattern;
 public class App {
     public static void main(String[] args) {
         LinkedListBuilder list = new LinkedListBuilder();
-
             list.add(1)
                 .add(2)
                 .add(3)
+                .addLast(4)
+                .addFirst(0)
                 .add(4)
                 .add(5)
+                .addLast(2)
                 .addAt(6, 3)
                 .addAt(7, 0)
                 .addAt(8, 6)
-                .addAt(9, 5);
+                .addAt(9, 5)
+                .addAt(10, 4)
+                .build();
 //        System.out.println(list.display());
     list.display();
     }
--- Semestri_2/Programim Algoritme/src/LikendListBuilderPattern/LinkedListBuilder.java
+++ Semestri_2/Programim Algoritme/src/LikendListBuilderPattern/LinkedListBuilder.java
@@ -3,24 +3,34 @@ package LikendListBuilderPattern;
 import Java_5_LinkedList.LinkedList2;
 
 public class LinkedListBuilder {
-        private LinkedList2 list = new LinkedList2();
+    private final LinkedList2 list = new LinkedList2();
 
-        public LinkedListBuilder add(Object element) {
-            list.add(element);
-            return this;
-        }
-
-        public LinkedListBuilder addAt(Object element, int index) {
-            list.addAt(element, index);
-            return this;
-        }
-
-        public LinkedList2 build() {
-            return list;
-        }
-
-        public void display() {
-            list.display();
-        }
+    public LinkedListBuilder add(Object element) {
+        list.add(element);
+        return this;
     }
+
+    public LinkedListBuilder addAt(Object element, int index) {
+        list.addAt(element, index);
+        return this;
+    }
+
+    public LinkedListBuilder addLast(Object element) {
+        list.addLast(element);
+        return this;
+    }
+
+    public LinkedListBuilder addFirst(Object element) {
+        list.addFirst(element);
+        return this;
+    }
+
+    public LinkedList2 build() {
+        return list;
+    }
+
+    public void display() {
+        list.display();
+    }
+}
 
