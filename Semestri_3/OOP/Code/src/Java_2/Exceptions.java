b7dd429efd26409ed517fba5d36b4e2bd57d8a3a
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2023-10-15 22:11:15 +0200

    Java 2

---
+++ Semestri_3/OOP/Code/src/Java_2/Exceptions.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Exceptions {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		System.out.print("Jepni piket e provimit te ndara me presje: "); //"1,2,a,4,5"
+		String piket = s.nextLine();
+
+		String [] piketVarg = piket.split(","); //{"1", "2", "a", ...}
+		int [] piketProv = new int[piketVarg.length];
+		
+		try {
+			for(int i = 0; i < piketVarg.length; i++)
+				piketProv[i] = Integer.parseInt(piketVarg[i]); //"1"->1, number format exception
+			//piketProv = {1, 2, 3, 4, 5}
+
+			double avg;
+			int sum = 0;
+
+			for(int i = 0; i < piketProv.length; i++)
+				sum += piketProv[i];
+
+			avg = (double)sum / piketProv.length;
+			System.out.println("Mesatarja: " + avg);
+		}
+		catch(NumberFormatException n) {
+			//kodi per trajtimin e gabimit NumberFormatException
+			System.out.println("Nuk mund te realizohet konvertimi i String->int");
+		}
+		catch(ArrayIndexOutOfBoundsException a) {
+			//kodi per trajtimin e gabimit ArrayIndexOutOfBoundsException
+			System.out.println("Ke tejkaluar kufinjte e vargut");
+		}
+		catch(Exception e) {
+			//kodi per trajtimin e gabimeve gjenerale
+			System.out.println("Gabim gjeneral");
+		}
+		finally {
+			System.out.println("Ekzekutimi do te arrije ne kete pike");
+		}
+		System.out.println("Programi perfundoi");
+	}
+}
