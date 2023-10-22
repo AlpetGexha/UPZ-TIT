6c299877ff61b10f175060b0a2a864804e708180
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2023-10-22 20:35:12 +0200

    Java 3

---
+++ Semestri_3/OOP/Code/src/Java_3/Mesatarja.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Mesatarja {
+
+	public static void main(String[] args) {
+		try {
+			BufferedReader reader = new BufferedReader(
+					new FileReader("ints.txt"));
+			int shuma = 0, count = 0;
+			while(reader.ready()) {
+				String rreshti = reader.readLine();
+				if(rreshti.equals(""))
+					continue;
+				String vargu [] = rreshti.split(" ,.");
+				for(int i = 0; i < vargu.length; i++) {
+					try {
+						int vlera = Integer.parseInt(vargu[i]);
+						shuma += vlera;
+						count++;
+					}
+					catch(NumberFormatException n) {
+						System.out.println("Nuk mund te konvertohet String ne int!");
+					}
+				}
+			}
+			reader.close();
+			if(count != 0) {
+				double mesatarja = (double)shuma / count;
+				System.out.println("Mesatarja = " + 
+						String.format("%.2f", mesatarja));
+			}
+			else {
+				System.out.println("Nuk ka pasur asnje numer ne fajllin hyres!");
+			}
+		}
+		catch(FileNotFoundException f) {
+			System.out.println("Ky fjall nuk u gjet!");
+		}
+		catch(IOException i) {
+			System.out.println("Gabim ne I/O!");
+		}
+	}
+}
+
