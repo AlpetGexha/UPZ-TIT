a6ff3002338736a4536a2433d8af47cb6711d5d2
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2023-11-10 20:58:44 +0100

    Detyra Kollofjumi 1

---
+++ Semestri_3/OOP/Code/src/Detyra_kollofjumi/Exception.java
@@ -0,0 +1,26 @@
+package Detyra_kollofjumi;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exception {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        try{
+            System.out.print("Jep nje numer: ");
+            int n = s.nextInt();
+
+            System.out.println(n);
+        }catch (InputMismatchException e){
+            System.out.println("Ju lutem jepni numer");
+        }
+
+        try{
+            int rezult = 10 / 0;
+            System.out.println(rezult);
+        }catch (ArithmeticException e){
+            System.out.println(e);
+        }
+    }
+}
