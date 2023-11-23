bd49bf0b33bc5794c319a8c99c2490b658b3da5e
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2023-11-23 21:46:29 +0100

    Create Sdasd.java

---
+++ Semestri_3/OOP/Code/src/Test_1/Sdasd.java
@@ -0,0 +1,23 @@
+package Test_1;
+
+import java.util.Scanner;
+
+public class Sdasd {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Engjellusha so ka kupton sen bash sen: ");
+        String piket = s.nextLine();
+        String[] piketVarg = piket.split(",");
+        int[] piketProv = new int[piketVarg.length];
+        for (int i = 0; i < piketVarg.length; i++)
+            piketProv[i] = Integer.parseInt(piketVarg[i]); //
+        double avg;
+        int sum = 0;
+        for (int i = 0; i <= piketProv.length; i++) //
+            sum += piketProv[i];
+
+        avg = (double) sum / piketProv.length; //
+        System.out.println("Mesatarja: " + avg);
+    }
+
+}
