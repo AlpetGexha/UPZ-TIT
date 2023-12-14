78ba3ca331cd6b5613df9e6ff9b7d5300d6b2391
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2023-12-14 22:00:45 +0100

    Java 11

---
+++ Semestri_3/OOP/Code/src/Java_11/Fjala.java
@@ -0,0 +1,29 @@
+package Java_11;
+
+public class Fjala extends Teksti {
+
+    private String permbajtja;
+
+    public Fjala(String p) {
+        super();
+        permbajtja = p;
+        gjatesia = permbajtja.length();
+    }
+
+    public String toString() {
+        return permbajtja + ": " + super.toString();
+    }
+
+    public String getPermbajtja() {
+        return permbajtja;
+    }
+
+    public int setPermbajtja(String p) {
+        int gjatesiaParaprake = permbajtja.length();
+
+        permbajtja = p;
+        gjatesia = permbajtja.length();
+
+        return Math.abs(gjatesiaParaprake - gjatesia);
+    }
+}
---
+++ Semestri_3/OOP/Code/src/Java_11/Teksti.java
@@ -0,0 +1,18 @@
+package Java_11;
+
+public class Teksti {
+
+    protected int gjatesia;
+
+    public Teksti() {
+        gjatesia = 0;
+    }
+
+    public String toString() {
+        return gjatesia + "";
+    }
+
+    public int getGjatesia() {
+        return gjatesia;
+    }
+}
