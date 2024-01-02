ebc01a93116570365b4502e98ad61bd388400eba
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2024-01-02 06:51:07 +0100

    Java 13

---
+++ Semestri_3/OOP/Code/src/Java_13/Bibliotekisti.java
@@ -0,0 +1,24 @@
+package Java_13;
+
+public class Bibliotekisti extends Person {
+
+    private double rroga;
+
+    public Bibliotekisti(String emri, String mbiemri, String adresa, double rroga) {
+        super(emri, mbiemri, adresa);
+        this.rroga = rroga;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", " + rroga;
+    }
+
+    public double getRroga() {
+        return rroga;
+    }
+
+    public void setRroga(double rroga) {
+        this.rroga = rroga;
+    }
+}
---
+++ Semestri_3/OOP/Code/src/Java_13/LibriInterface.java
@@ -0,0 +1,14 @@
+package Java_13;
+
+public interface LibriInterface {
+
+	public String getISBN();
+	
+	public String getTitulli();
+	
+	public String getAutori();
+	
+	public int getNumriKopjeve();
+	
+	public void setNumriKopjeve(int n);
+}
