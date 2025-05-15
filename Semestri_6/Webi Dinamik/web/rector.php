b179bd4067aef886ca4779db6e58df263bdcf949
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2025-05-16 01:09:04 +0200

    Update rector.php

---
+++ Semestri_6/Webi Dinamik/web/rector.php
@@ -0,0 +1,30 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
+use Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/',
+    ])
+    ->withSkip([
+        '*/vendor/*',
+    ])
+    ->withRules([
+        TypedPropertyFromStrictConstructorRector::class,
+        ReturnNeverTypeRector::class,
+        ExplicitNullableParamTypeRector::class,
+        NullToStrictStringFuncCallArgRector::class,
+    ])
+    // here we can define, what prepared sets of rules will be applied
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+    );
+//    ->withPhpSets();
